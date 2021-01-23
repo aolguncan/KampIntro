ccfeb2c6cfbbff99f7f5d62b7abd7b5210c9fc35
Atila OLGUNCAN <atila.olguncan@gmail.com>
2021-01-24 01:14:50 +0300

    23 Ocak Ders Sonu

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //nameing convention
+        //xxxxxxxxxxxxxxxxxxxxxxxxxxx
+        public void Ekle()
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi!");
+        }
+        public void Ekle2(string urunAdi,string aciklama,double fiyat)
+        {
+            Console.WriteLine("Tebrikler Sepete Eklendi" + urunAdi);
+        }
+    }
+}
